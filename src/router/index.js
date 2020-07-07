7f6d5b77a31bdf3fa7ae16ec215bb7ff399a7933
Pizhong <15626160293@163.com>
2020-07-07 21:48:16 +0800

    完成了权限功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Login from '../components/Login.vue';
 import Home from '../components/Home.vue';
 import Welcome from '../components/Welcome.vue';
 import Users from '../components/user/Users.vue';
+import Rights from '../components/power/Rights.vue';
+import Roles from '../components/power/Roles.vue';
 
 Vue.use(VueRouter);
 
@@ -14,8 +16,11 @@ const routes = [
     path: '/home',
     component: Home,
     redirect: '/welcome',
-    children: [{path: '/welcome', component: Welcome},
+    children: [
+      {path: '/welcome', component: Welcome},
       {path: '/users', component: Users},
+      {path: '/rights', component: Rights},
+      {path: '/roles', component: Roles},
     ],
   },
 ];
@@ -37,5 +42,12 @@ router.beforeEach((to, from, next) => {
   if (!tonkenStr) return next('/login');
   next();
 });
+
+
+// 解决ElementUI导航栏中的vue-router在3.0版本以上重复点菜单报错问题
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch((err) => err);
+};
 
 export default router;
